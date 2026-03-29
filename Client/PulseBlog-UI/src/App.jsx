b4a2e7e656970d1fccfddff797f558982dfd6694
isidcode <sidharthsharma1410@gmail.com>
2026-03-29 08:47:44 +0530

    updated app

--- Client/PulseBlog-UI/src/App.jsx
+++ Client/PulseBlog-UI/src/App.jsx
@@ -1,25 +1,27 @@
 // src/App.jsx
-import { Route, Routes } from "react-router-dom"
-import Navbar       from "./components/navbar.component"
-import UserAuthForm from "./pages/userAuthForm.page"
-import Editor       from "./pages/editor.page"
-import HomePage     from "./pages/home.page"
-import SearchPage   from "./pages/search.page" // Import Search
-import BlogPage     from "./pages/blog.page"   // Import Blog
+import { Route, Routes }   from "react-router-dom"
+import Navbar              from "./components/navbar.component"
+import UserAuthForm        from "./pages/userAuthForm.page"
+import Editor              from "./pages/editor.page"
+import HomePage            from "./pages/home.page"
+import SearchPage          from "./pages/search.page"
+import BlogPage            from "./pages/blog.page"
+import UserProfilePage     from "./pages/userProfile.page"
+import LoginSuccess        from "./pages/loginSuccess.page"    // ← NEW
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/editor" element={<Editor />} />
+      <Route path="/editor"        element={<Editor />} />
+      <Route path="/login-success" element={<LoginSuccess />} />  {/* ← NEW — outside Navbar */}
 
       <Route path="/" element={<Navbar />}>
-        <Route index         element={<HomePage />} />
-        <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-        
-        {/* Add the new routes here */}
-        <Route path="search/:query" element={<SearchPage />} />
-        <Route path="blog/:blog_id" element={<BlogPage />} />
+        <Route index                 element={<HomePage />}                        />
+        <Route path="signin"         element={<UserAuthForm type="sign-in" />}     />
+        <Route path="signup"         element={<UserAuthForm type="sign-up" />}     />
+        <Route path="search/:query"  element={<SearchPage />}                      />
+        <Route path="blog/:blog_id"  element={<BlogPage />}                        />
+        <Route path="user/:username" element={<UserProfilePage />}                 />
       </Route>
     </Routes>
   )
