388ad2d601b45a626e0636ab14c5942b3469fb5e
Sidharth Sharma <161471127+isidcode@users.noreply.github.com>
2026-04-16 21:06:18 +0530

    Delete Server directory

--- Server/src/utils/Asynchandler.js
+++
@@ -1,9 +0,0 @@
-//Best clean method
-const Asynchandler = function(handler){
-    return (req,res,next)=>{
-        Promise.resolve(handler(req,res,next))
-        .catch((error)=>next(error))
-    }
-}
-
-export {Asynchandler}
